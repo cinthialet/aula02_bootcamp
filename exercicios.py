925c4e9692652ba4b52a1da1d238ddb1d28abeba
cinthialet <cithsantos@gmail.com>
2024-02-25 16:07:52 -0300

    execicio 01 adicionado

--- exercicios.py
+++ exercicios.py
@@ -1,13 +1,21 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_1 = int(input("Digite o primeiro numero: "))
+numero_2 = int(input("Digite o segundo numero: "))
+resultado_soma = numero_1 + numero_2
+print("A soma é:", resultado_soma)
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-primeiro_n_inteiro = int(input("Coloque o primeiro numero inteiro = "))
-segundo_n_inteiro = int(input("Coloque o segundo numero inteiro = "))
-resultado = primeiro_n_inteiro // segundo_n_inteiro
-print(resultado)
+
+#primeiro_n_inteiro = int(input("Coloque o primeiro numero inteiro = "))
+#segundo_n_inteiro = int(input("Coloque o segundo numero inteiro = "))
+#resultado = primeiro_n_inteiro // segundo_n_inteiro
+#print("O resultado da divisão é:", resultado)
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 
